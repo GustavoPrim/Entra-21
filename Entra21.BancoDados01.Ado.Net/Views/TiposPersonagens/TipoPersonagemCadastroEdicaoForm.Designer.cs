5aa3dfeb2732db7810d269f90cee70ca1e467a74
GustavoPrim <gugahprm@gmail.com>
2022-07-14 22:08:13 -0300

    Feito exemplo de Banco de dados com windows forms

---
+++ Entra21.BancoDados01.Ado.Net/Views/TiposPersonagens/TipoPersonagemCadastroEdicaoForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Entra21.BancoDados01.Ado.Net.Views.TiposPersonagens
+{
+    partial class TipoPersonagemCadastroEdicaoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTipo = new System.Windows.Forms.TextBox();
+            this.labelTipo = new System.Windows.Forms.Label();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 0;
+            // 
+            // textBoxTipo
+            // 
+            this.textBoxTipo.Location = new System.Drawing.Point(12, 32);
+            this.textBoxTipo.Name = "textBoxTipo";
+            this.textBoxTipo.Size = new System.Drawing.Size(388, 27);
+            this.textBoxTipo.TabIndex = 1;
+            this.textBoxTipo.TextChanged += new System.EventHandler(this.textBoxTipo_TextChanged);
+            // 
+            // labelTipo
+            // 
+            this.labelTipo.AutoSize = true;
+            this.labelTipo.Location = new System.Drawing.Point(12, 9);
+            this.labelTipo.Name = "labelTipo";
+            this.labelTipo.Size = new System.Drawing.Size(39, 20);
+            this.labelTipo.TabIndex = 2;
+            this.labelTipo.Text = "Tipo";
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(324, 70);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(76, 29);
+            this.buttonSalvar.TabIndex = 3;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(243, 70);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 29);
+            this.buttonCancelar.TabIndex = 4;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
+            // 
+            // TipoPersonagemCadastroEdicaoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 114);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.labelTipo);
+            this.Controls.Add(this.textBoxTipo);
+            this.Controls.Add(this.label1);
+            this.Name = "TipoPersonagemCadastroEdicaoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro/Edição Tipo de Personagem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBoxTipo;
+        private Label labelTipo;
+        private Button buttonSalvar;
+        private Button buttonCancelar;
+    }
+}
---
+++ Entra21.BancoDados01.Ado.Net/Views/TiposPersonagens/TipoPersonagemListagemForm.cs
@@ -0,0 +1,66 @@
+﻿using Entra21.BancoDados01.Ado.Net.Services;
+
+namespace Entra21.BancoDados01.Ado.Net.Views.TiposPersonagens
+{
+    public partial class TipoPersonagemListagemForm : Form
+    {
+        private TipoPersonagemService tipoPersonagemService;
+        public TipoPersonagemListagemForm()
+        {
+            InitializeComponent();
+
+            //Instanciado objeto da classe TipoPersonagemService para permitir
+            // - Listar todos
+            // - Editar
+            // - Apagar
+            tipoPersonagemService = new TipoPersonagemService();
+        }
+
+        private void TipoPersonagemListagemForm_Load(object sender, EventArgs e)
+        {
+            AtualizarRegistrosDataGrideView();
+        }
+
+        private void buttonCadastrar_Click(object sender, EventArgs e)
+        {
+            var tipoPersonagemCadastroEdicaoForm = new TipoPersonagemCadastroEdicaoForm();
+
+            tipoPersonagemCadastroEdicaoForm.ShowDialog();
+
+            AtualizarRegistrosDataGrideView();
+        }
+
+        private void AtualizarRegistrosDataGrideView()
+        {
+            // Obter todos os tipos de personagens cadastrados na tabela tipos_personagens
+            var tiposPersonagens = tipoPersonagemService.ObterTodos();
+
+            // Removido todos os registros do dataGrideView
+            dataGridView1.Rows.Clear();
+
+            // Percorrido todos os tipos de personagens encontrados para
+            // adicionar no DataGrideView permitindo o usuário visualiza-los
+            for (var i = 0; i < tiposPersonagens.Count; i++)
+            {
+                var tipoPersonagem = tiposPersonagens[i];
+
+                dataGridView1.Rows.Add(new object[]
+                {
+                    tipoPersonagem.Id,
+                    tipoPersonagem.Tipo
+                });
+            }
+        }
+
+        private void buttonApagar_Click(object sender, EventArgs e)
+        {
+            var id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+
+            tipoPersonagemService.Apagar(id);
+
+            AtualizarRegistrosDataGrideView();
+
+            MessageBox.Show("Registro apagado com sucesso");
+        }
+    }
+}
