b370d4bdbe5a5d2a6e91194ffdca31e3453c2131
GustavoPrim <gugahprm@gmail.com>
2022-07-18 22:12:00 -0300

    Iniciado primeiro exercício de TabelasRelacionadas com Banco de Dados

---
+++ Entra21.BancoDados01.Ado.Net/Exercicio/Models/Cidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.BancoDados01.Ado.Net.Exercicio.Models
+{
+    internal class Cidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int QuantidadeHabitantes { get; set; }
+        public DateTime DataHoraFundacao { get; set; }
+    }
+}
